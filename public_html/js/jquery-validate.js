752fcfa84cd553ab79e58c6786d8f50b3b6b91e8
Chloe Alnaji <caedeal@gmail.com>
2018-09-18 12:44:41 -0600

    Revert "todo"
    This reverts commit a5c650b046108cda7d03bd79534a351247e34a3c.

--- public_html/js/jquery-validate.js
+++ public_html/js/jquery-validate.js
@@ -55,7 +55,6 @@ $(document).ready(function(){
 
 		// AJAX submit the form data to back end if rules pass
 		submitHandler: function(form) {
-			console.log("about to submit");
 			$("#contact-form").ajaxSubmit({
 				type: "POST",
 				url: $("#contact-form").attr("action"),
